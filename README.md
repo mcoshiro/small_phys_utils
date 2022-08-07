bc8df15d0a3ecbe3d482e68ea934637eda07e5e0
Michael Oshiro <oshiro@ucsb.edu>
2022-08-07 12:24:50 -0700

    First commit of small_phys_utils

---
+++ README.md
@@ -0,0 +1,1 @@
+A place to collect various small scripts and utilities used during physics analysis and other tasks.
---
+++ set_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#export SMALL_PHYS_UTILS_DIR=$(dirname $(readlink -e "$BASH_SOURCE"))
+#export PATH=$SMALL_PHYS_UTILS_DIR/bin::$PATH
+#export PYTHONPATH=$SMALL_PHYS_UTILS_DIR/libs:$PYTHONPATH
